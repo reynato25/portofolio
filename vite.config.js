6a0c022cd44d0177181229806fc430d0b76486ed
Reynato <reynatomuliawan@gmail.com>
2025-07-15 02:01:51 +0700

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    port: 8080
+  },
+  css: {
+    preprocessorOptions: {
+        scss: {
+          silenceDeprecations: [
+            'import',
+            'mixed-decls',
+            'color-functions',
+            'global-builtin',
+            'legacy-js-api',
+          ],
+        },
+    },
+  },
+})
